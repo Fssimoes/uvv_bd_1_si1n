506ffb6ce61e4933bc19f6cc3d2516ae52e4e88d
Felipe Simões <117595315+Fssimoes@users.noreply.github.com>
2022-11-14 10:17:14 -0300

    Update si1n_202203623_postgres_hr.sql

--- pset1/si1n_202203623_postgres_hr.sql
+++ pset1/si1n_202203623_postgres_hr.sql
@@ -25,10 +25,10 @@ CREATE DATABASE uvv
     LC_COLLATE = 'en_US.UTF-8'
     LC_CTYPE = 'en_US.UTF-8'
     CONNECTION LIMIT = -1;
-  
-   	-- SCHEMA: hr
 
-DROP SCHEMA IF EXISTS hr ;
+\c uvv
+
+   	-- SCHEMA: hr
 
 CREATE SCHEMA IF NOT EXISTS hr
     AUTHORIZATION felipe;
